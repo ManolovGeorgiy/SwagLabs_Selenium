2dab7a58ad909ec068b93b0161cb8f66ac80d37f
Georgiy <g.manolov@web.de>
2024-03-09 18:21:19 +0100

    update

--- src/test/java/e2e/pages/YourCartInfoPage.java
+++ src/test/java/e2e/pages/YourCartInfoPage.java
@@ -1,5 +1,6 @@
 package e2e.pages;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,18 +11,36 @@ public class YourCartInfoPage extends ProductsInfoPage{
     }
     @FindBy(xpath = "//div[@class='header_label']")
     public WebElement header;
-    @FindBy(xpath = "//*[@id='remove-sauce-labs-backpack']")
+    @FindBy(xpath = "//div[@class=/cart_list']")
+    public WebElement cartList;
+    @FindBy(xpath = "//div[@class='inventory_item_name']")
+    public WebElement inventoryItemName;
+    @FindBy(xpath = "//div[@class='inventory_item_price']")
+    public WebElement inventoryItemPrice;
+    @FindBy(xpath = "//*[@class='btn btn_secondary btn_small cart_button']")
     WebElement removeButton;
     @FindBy(xpath = "//*[@id='continue-shopping']")
     WebElement continueShoppingButton;
     @FindBy(xpath = "//*[@id='checkout']")
     WebElement checkoutButton;
     public void waitForOpen(){
+        getWait().forVisibility(cartList);
+        getWait().forVisibility(inventoryItemName);
+        getWait().forVisibility(inventoryItemPrice);
         getWait().forVisibility(removeButton);
         getWait().forVisibility(continueShoppingButton);
         getWait().forClickable(checkoutButton);
     }
-    public void clickCheckoutButton(){
+    public void InventoryAllItemsName(String[] args) {
+
+    }
+    public void InventoryItemPrice(String[] args){
+
+
+    }
+
+
+        public void clickCheckoutButton(){
         checkoutButton.click();
     }
 }
